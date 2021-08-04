7f1698786a75427edcfb41a69e5ff3cb380596ef
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2021-08-04 15:59:10 -0400

    Update Teamviewer.ps1

--- Teamviewer.ps1
+++ Teamviewer.ps1
@@ -81,3 +81,49 @@ function Get-TVIncomingLog_Top10Duration{
         $obj | Sort-Object Duration -Bottom 10
     }
 }
+
+function Get-TVIncomingLog_Unique{
+    [CmdletBinding()]
+    param(
+        $File,
+        [switch]$IncomingID,
+        [switch]$DisplayName,
+        [switch]$LoggedOnUser
+
+    )
+    $logs = get-content $file
+
+    $obj = @()
+    $logs = $logs -replace (' ','_')
+    $obj = foreach($log in $logs){
+        $dur = ''
+        $log = $log -split "\s+"
+        $log = $log -replace ('_',' ')
+        try{
+            $log[2]= [datetime]::ParseExact($log[2],'dd-MM-yyyy HH:mm:ss', $null)
+        } catch{ }
+        try{
+            $log[3]= [datetime]::ParseExact($log[3],'dd-MM-yyyy HH:mm:ss', $null) 
+        } catch { }
+        $dur = New-TimeSpan -start $log[2] -end $log[3] -ErrorAction SilentlyContinue
+        [PSCustomObject]@{
+            IncomingID = $log[0]
+            DisplayName = $log[1]
+            StartDate = $log[2]
+            EndDate = $log[3]
+            Duration = [string]$dur.ToString("dd'd.'hh'h:'mm'm:'ss's'")
+            LoggedOnUser = $log[4]
+            ConnectionType = $log[5]
+            ConnectionID = $log[6]
+        }
+    }
+    if($IncomingID){
+        $obj | Sort-Object -Property incomingid -Unique | select-object incomingid
+    }
+    elseif($DisplayName){
+        $obj | Sort-Object -Property displayname -Unique | select-object displayname
+    }
+    elseif($LoggedOnUser){
+        $obj | Sort-Object -Property LoggedOnUser -Unique | select-object LoggedOnUser
+    }
+}
